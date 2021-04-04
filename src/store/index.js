5431efd5c547fa3131ed509780bd9a7b9dbb3f04
jensengbg-elliot-berggren <elliot.berggren@hotmail.com>
2021-04-04 19:42:20 +0200

    Added route and vue file for flow page

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
-//import router from './../router'
+import router from './../router'
 //import CryptoJS from 'crypto-js';
 
 Vue.use(Vuex)
@@ -39,7 +39,7 @@ export default new Vuex.Store({
       sessionStorage.setItem('userkey', resp.data.userkey);
 
       // Route to /passwords
-      //router.push('/flow')
+      router.push('/flow')
     
   }
 },
---
+++ src/views/Flow.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="flow">
+
+  </section>
+</template>
+
+<script>
+export default {
+    name: "Flow page"
+}
+</script>
+
+<style>
+
+</style>
