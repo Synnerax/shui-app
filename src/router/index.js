b127f423200e4d58972db560c49b424d3fc1c19d
jensengbg-elliot-berggren <elliot.berggren@hotmail.com>
2021-04-04 19:19:37 +0200

    Added frontend page for login Connected frontend with backend

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/RegisterAccount.vue')
+  },
+  {
+    path: '/sign-in',
+    name: 'sign-in page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
